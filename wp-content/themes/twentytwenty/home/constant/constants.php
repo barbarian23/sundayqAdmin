56106b20d388752b20be09fdc26f24a15a18cabc
barbarian23 <xuanchien9a4@gmail.com>
2020-07-05 20:30:11 +0700

    add constant

--- wp-content/themes/twentytwenty/home/constant/constants.php
+++ wp-content/themes/twentytwenty/home/constant/constants.php
@@ -1,4 +1,8 @@
 <?php
-
+	global $nameDashboard = "Trang quản trị";
+	global $manageTeacher = "Quản lý giáo viên";
+	global $manageAccount = "Quản lý tài khoản";
+	global $manageLecture = "Quản lý bài giảng";
+	global $manageLecture = "Quản lý bài giảng";
 
 ?>
